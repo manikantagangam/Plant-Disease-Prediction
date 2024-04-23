dfda8fb591441dad08cd99e5087e99a0eb3b7fe2
manikantagangam <manikantagangam@gmail.com>
2024-04-23 19:32:33 +0530

    changes made to prevent saving of images in upload folder

--- app.py
+++ app.py
@@ -1,7 +1,125 @@
+# # -*- coding: utf-8 -*-
+
+# from __future__ import division, print_function
+# # coding=utf-8
+# import sys
+# import os
+# import glob
+# import re
+# import numpy as np
+# import tensorflow as tf
+
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
+
+# config = ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 0.5
+# config.gpu_options.allow_growth = True
+# session = InteractiveSession(config=config)
+# # Keras
+# from tensorflow.keras.applications.resnet50 import preprocess_input
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+
+# # Flask utils
+# from flask import Flask, redirect, url_for, request, render_template
+# from werkzeug.utils import secure_filename
+# #from gevent.pywsgi import WSGIServer
+
+# # Define a flask app
+# app = Flask(__name__)
+
+# # Model saved with Keras model.save()
+# # MODEL_PATH ='model_inception.h5'
+# MODEL_PATH ='Tomato_Disease_Detection.h5'
+# # Tomato_Disease_Detection.h5
+
+# # Load your trained model
+# model = load_model(MODEL_PATH)
+
+
+
+
+# def model_predict(img_path, model):
+#     print(img_path)
+#     img = image.load_img(img_path, target_size=(224, 224))
+
+#     # Preprocessing the image
+#     x = image.img_to_array(img)
+#     # x = np.true_divide(x, 255)
+#     ## Scaling
+#     x=x/255
+#     x = np.expand_dims(x, axis=0)
+   
+
+#     # Be careful how your trained model deals with the input
+#     # otherwise, it won't make correct prediction!
+#    # x = preprocess_input(x)
+
+#     preds = model.predict(x)
+#     preds=np.argmax(preds, axis=1)
+#     if preds==0:
+#         preds="Bacterial_spot"
+#     elif preds==1:
+#         preds="Early_blight"
+#     elif preds==2:
+#         preds="Late_blight"
+#     elif preds==3:
+#         preds="Leaf_Mold"
+#     elif preds==4:
+#         preds="Septoria_leaf_spot"
+#     elif preds==5:
+#         preds="Spider_mites Two-spotted_spider_mite"
+#     elif preds==6:
+#         preds="Target_Spot"
+#     elif preds==7:
+#         preds="Tomato_Yellow_Leaf_Curl_Virus"
+#     elif preds==8:
+#         preds="Tomato_mosaic_virus"
+#     else:
+#         preds="Healthy"
+        
+    
+    
+#     return preds
+
+
+# @app.route('/', methods=['GET'])
+# def index():
+#     # Main page
+#     return render_template('index.html')
+
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         # Get the file from post request
+#         f = request.files['file']
+
+#         # Save the file to ./uploads
+#         basepath = os.path.dirname(__file__)
+#         file_path = os.path.join(
+#             basepath, 'uploads', secure_filename(f.filename))
+#         f.save(file_path)
+
+#         # Make prediction
+#         preds = model_predict(file_path, model)
+#         result=preds
+#         return result
+#     return None
+
+
+# if __name__ == '__main__':
+#     app.run(port=5001,debug=True)
+
+
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
 # -*- coding: utf-8 -*-
 
 from __future__ import division, print_function
-# coding=utf-8
 import sys
 import os
 import glob
@@ -16,48 +134,34 @@ config = ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.5
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
+
 # Keras
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-#from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template
 
 # Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
-# MODEL_PATH ='model_inception.h5'
+# Model path
 MODEL_PATH ='Tomato_Disease_Detection.h5'
-# Tomato_Disease_Detection.h5
 
 # Load your trained model
 model = load_model(MODEL_PATH)
 
-
-
-
-def model_predict(img_path, model):
-    print(img_path)
-    img = image.load_img(img_path, target_size=(224, 224))
-
+def model_predict(img, model):
     # Preprocessing the image
     x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    ## Scaling
-    x=x/255
     x = np.expand_dims(x, axis=0)
-   
+    x = x / 255.0  # Normalize pixel values
 
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-   # x = preprocess_input(x)
-
+    # Make prediction
     preds = model.predict(x)
-    preds=np.argmax(preds, axis=1)
+    preds = np.argmax(preds, axis=1)
+    
     if preds==0:
         preds="Bacterial_spot"
     elif preds==1:
@@ -84,30 +188,91 @@ def model_predict(img_path, model):
     return preds
 
 
-@app.route('/', methods=['GET'])
+    # Map prediction index to class name
+    # class_names = [
+    #     "Bacterial_spot", "Early_blight", "Late_blight", "Leaf_Mold",
+    #     "Septoria_leaf_spot", "Spider_mites Two-spotted_spider_mite",
+    #     "Target_Spot", "Tomato_Yellow_Leaf_Curl_Virus", "Tomato_mosaic_virus",
+    #     "Healthy"
+    # ]
+    # pred_class = class_names[preds[0]]
+    
+    # return pred_class
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Main page
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        
+        # If the user does not select a file, the browser submits an empty file without a filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        # Check if file is present
+        if file:
+            # Read the file directly without saving
+            img = image.load_img(file, target_size=(224, 224))
+            
+            # Make prediction
+            prediction = model_predict(img, model)
+            return render_template('result.html', prediction=prediction)
+    
+    # Render the upload form
     return render_template('index.html')
 
+from PIL import Image
+from io import BytesIO
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
+from PIL import Image
+from io import BytesIO
+
+def get_class_name(class_index):
+    class_names = [
+        "Bacterial_spot", "Early_blight", "Late_blight", "Leaf_Mold",
+        "Septoria_leaf_spot", "Spider_mites Two-spotted_spider_mite",
+        "Target_Spot", "Tomato_Yellow_Leaf_Curl_Virus", "Tomato_mosaic_virus",
+        "Healthy"
+    ]
+    return class_names[class_index]
+
+@app.route('/predict', methods=['POST'])
+def predict():
     if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+        # Get the uploaded file
+        file = request.files['file']
+        
+        # Read the file data
+        file_data = file.read()
+        
+        # Load the image using PIL
+        img = Image.open(BytesIO(file_data))
+        img = img.resize((224, 224))  # Resize the image if needed
+        
+        # Convert the image to a numpy array and preprocess it if required
+        img_array = np.array(img) / 255.0  # Example: Normalize pixel values
+        
+        # Make prediction using your model
+        preds = model.predict(np.expand_dims(img_array, axis=0))
+        predicted_class_index = np.argmax(preds)
+        
+        # Get the class name corresponding to the predicted class index
+        predicted_class_name = get_class_name(predicted_class_index)
+        
+        # Return the predicted class name as a response
+        return predicted_class_name
 
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
 
-        # Make prediction
-        preds = model_predict(file_path, model)
-        result=preds
-        return result
-    return None
+
 
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
+    app.run(port=5001, debug=True)
+
+
